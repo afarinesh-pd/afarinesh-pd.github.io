3b64d449f892ffc548be5bb43491bd43157216a6
afarinesh-pd <parsa.babaloo1300@gmail.com>
2026-09-16 00:49:31 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://afarinesh-api.parsababalo1403.workers.dev/";
+const API_URL = "https://afarinesh-api.parsababalo1403.workers.dev";
 
 document.addEventListener("DOMContentLoaded", function () {
     
